f2fbe27a43a84a09ffd2e674b0a10871421c2422
Daniel Caaz <dcaazy@gmail.com>
2018-07-19 18:30:41 -0700

    Fully implement character rendering with color options

--- classes/character_renderer.lua
+++ classes/character_renderer.lua
@@ -1,18 +1,89 @@
-function character(body, head, x, y, tick)
+function character(body, head, x, y, tick, body_color, head_color, hair_color,skin)
   palt(0,false)
   palt(1,true)
-  -- shadows
-  local heady = y-3
-  if tick then
-    body += 16
-    spr(48,x,y)
-    heady -= 1
-  else
-    spr(32,x,y)
+  -- bounce animation
+  local hy = ((tick%40 < 20) and 1 or 0)
+  local by = (((tick+4)%40 < 20) and 1 or 0)
+
+  -- sspr sx sy sw sh dx dy [dw dh] [flip_x] [flip_y]
+  local sx = body % 16 * 8
+  local sy = 16
+  -- outline, body
+  sspr(0,24,8,5,x,y+by)
+  -- outline, legs
+  sspr(0,24+5,8,3,x,y+5)
+  -- outline, head
+  spr(16+head,x,y-4+hy)
+
+  for i = 1, 2 do
+    if body_pals[body+1] then
+      local f = body_pals[body+1][i]
+      if f then
+        pal(f,pals[body_color][i])
+      end
+    end
   end
-  spr(16+head,x,heady)
-  -- sprite
-  spr(32+body,x,y)
-  spr(head,x,heady)
-  palt()
+  if skin then
+    pal(15,4)
+  end
+  -- legs
+  sspr(sx,sy+5,8,3,x,y+5)
+  -- body
+  sspr(sx,sy,8,5,x,y+by)
+
+  pal()
+  palt(0,false)
+  palt(1,true)
+
+  for i = 1, 4 do
+    if head_pals[head+1] then
+      local f = head_pals[head+1][i]
+      if f then
+        pal(f,pals[(i <= 2 and hair_color or head_color)][(i-1)%2+1])
+      end
+    end
+  end
+  if skin then
+    pal(15,4)
+  end
+  -- head
+  spr(head,x,y-4+hy)
+  pal()
 end
+
+-- colors to swap?
+body_pals = {
+  {9},
+  {3},
+  {11},
+  {7,6},
+  {10,4},
+  {5,13},
+  {7,6},
+  {12}
+}
+-- hair a, hair b, item a, item b
+head_pals = {
+  {4,5},
+  {10,9},
+  {10,9,11,3},
+  {1,1,7,6},
+  {4,1,13,5},
+  {1,1,8,2},
+  {1,1,7,6},
+  {12,13}
+}
+
+pals = {
+  {8,2},
+  {9,4},
+  {10,9},
+  {11,3},
+  {12,1},
+  {14,2},
+  {7,6},
+  {6,13},
+  {13,1},
+  -- {5,1},
+  -- {4,5}
+}
--- modules/test.lua
+++ modules/test.lua
@@ -1,30 +1,47 @@
 .add('test', {
   init = function(this)
     this.tick = 0
-    this.head = 1
-    this.body = 1
+    this.head = 0
+    this.body = 0
+    this.body_color = 1
+    this.hair_color = 1
+    this.skin_color = 1
+    this.head_color = 1
   end,
   update = function(this)
-
     if btnp(0) then
-      this.head -= 1
-    elseif btnp(1) then
-      this.head += 1
+      this.head = (this.head%8)+1
+    end
+    if btnp(1) then
+      this.body = (this.body%8)+1
     end
     if btnp(2) then
-      this.body -= 1
-    elseif btnp(3) then
-      this.body += 1
+      this.head_color = (this.head_color%9)+1
+    end
+    -- if btnp(3) then
+    --   this.skin_color = (this.skin_color%9)+1
+    -- end
+    if btnp(4) then
+      this.body_color = (this.body_color%9)+1
+    end
+    if btnp(5) then
+      this.hair_color = (this.hair_color%9)+1
     end
   end,
   draw = function(this)
     this.tick += 1
     -- rectfill()
     rectfill(0,0,127,127,7)
-    character(this.head,this.body,25,25, (this.tick%20<10))
+    character(this.body-1,this.head-1,25,40, this.tick,this.body_color,this.head_color,this.hair_color)--this.tick%20<10))
+    print('head: '..this.head,1,1,0)
+    print('body: '..this.body,1,7,0)
+    print('body color: '..this.body_color,1,13,0)
+    print('head color: '..this.head_color,1,25,0)
+    print('hair color: '..this.hair_color,1,19,0)
+    -- print('skin color: '..this.skin_color,1,25,0)
 
     for x = 20, 40 do
-      for y = 20, 40 do
+      for y = 35, 50 do
         local pixel = pget(x,y)
         for cx = 1, 2 do
           for cy = 1, 2 do
